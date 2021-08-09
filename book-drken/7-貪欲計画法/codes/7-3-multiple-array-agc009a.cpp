b4e3088515c5afb452d19dcfb5ad71e7e30c7387
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2021-08-09 22:54:37 +0900

    greedy codes

---
+++ book-drken/7-貪欲計画法/codes/7-3-multiple-array-agc009a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// https://atcoder.jp/contests/agc009/tasks/agc009_a
+
+int main() {
+    // 入力
+    int N;
+    cin >> N;
+    vector<long long> A(N), B(N);
+    for (int i = 0; i < N; ++i) cin >> A[i] >> B[i];
+
+    // 答え
+    long long sum = 0;
+    for (int i = N - 1; i >= 0; --i) {
+        A[i] += sum;  // 前回までの操作回数を足す
+        long long amari = A[i] % B[i];
+        long long D = 0;
+        if (amari != 0) D = B[i] - amari;
+        sum += D;
+    }
+    cout << sum << endl;
+}
