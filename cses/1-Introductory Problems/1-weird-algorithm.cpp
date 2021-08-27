0d1e0393f0574b1b18006f597610e9d052874fa0
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2021-08-27 22:48:21 +0900

    cses intro

---
+++ cses/1-Introductory Problems/1-weird-algorithm.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+//#include <atcoder/all>
+// using namespace atcoder;
+using namespace std;
+
+// debug
+#define printv(v)         \
+    for (auto x : v) {    \
+        cerr << x << " "; \
+    }                     \
+    cerr << endl;
+
+#define print2d(v, n, m)                \
+    for (int i = 0; i < (n); i++) {     \
+        for (int j = 0; j < (m); j++) { \
+            cerr << v[i][j] << " ";     \
+        }                               \
+        cerr << endl;                   \
+    }
+
+// func
+#define rep(i, first, last) for (int i = (first); i < (last); i++)
+#define srtv(v) sort((v).begin(), (v).end())
+
+// template
+template <class T>
+void chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+    }
+}
+template <class T>
+void chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+    }
+}
+
+// Driver function to sort the vector elements
+// by second element of pairs
+bool sort_by_fir_desc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second > b.second);
+}
+bool sort_by_sec_asc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second < b.second);
+}
+bool sort_by_sec_desc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second > b.second);
+}
+
+// type
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef priority_queue<int> pqi;                              // greater
+typedef priority_queue<int, vector<int>, greater<int>> pqli;  // less
+typedef priority_queue<long long, vector<long long>, greater<long long>> pqlll;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+
+// const
+const long long INF_9 = 1001002009;
+
+/*-----------------------------------
+        Coding Starts Here
+------------------------------------*/
+
+void solve() {
+    // in
+    ll n;
+    vi ans;
+    cin >> n;
+
+    while (n != 1) {
+        cout << n << " ";
+        if (n % 2)
+            n = n * 3 + 1;
+        else
+            n /= 2;
+    }
+    cout << n << endl;
+}
+
+int main() {
+    // make input and output more efficient
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--) solve();
+    return 0;
+}
