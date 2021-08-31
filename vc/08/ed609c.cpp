de7c8a8a3d9c3c9e5d029e29ad9c7972fea08cc0
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2021-09-01 01:53:53 +0900

    ed609

---
+++ vc/08/ed609c.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+
+//#include <atcoder/all>
+// using namespace atcoder;
+using namespace std;
+
+// debug
+#define printv(v)         \
+    for (auto x : v) {    \
+        cerr << x << " "; \
+    }                     \
+    cerr << endl;
+
+#define print2d(v, n, m)                \
+    for (int i = 0; i < (n); i++) {     \
+        for (int j = 0; j < (m); j++) { \
+            cerr << v[i][j] << " ";     \
+        }                               \
+        cerr << endl;                   \
+    }
+
+// func
+#define rep(i, last) for (int i = 0; i < (last); i++)
+#define srtv(v) sort((v).begin(), (v).end())
+
+// template
+template <class T>
+void chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+    }
+}
+template <class T>
+void chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+    }
+}
+
+// Driver function to sort the vector elements
+// by second element of pairs
+bool sort_by_fir_desc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second > b.second);
+}
+bool sort_by_sec_asc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second < b.second);
+}
+bool sort_by_sec_desc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second > b.second);
+}
+
+// type
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef priority_queue<int> pqi;                              // greater
+typedef priority_queue<int, vector<int>, greater<int>> pqli;  // less
+typedef priority_queue<long long, vector<long long>, greater<long long>> pqlll;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+
+// const
+const long long INF_9 = 1001002009;
+
+/*-----------------------------------
+        Coding Starts Here
+------------------------------------*/
+
+void solve() {
+    // in
+    int n;
+    cin >> n;
+    vll a(n);
+    ll s = 0;
+    rep(i, n) {
+        cin >> a[i];
+        s += a[i];
+    }
+    srtv(a);
+    printv(a);
+    ll base = s / n;
+    ll cnt = n - s % n;
+    // cout << s << " " << base << " " << cnt << endl;
+
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (base == a[i]) cnt--;
+        if (cnt == 0 and s % n > 0) base++;
+
+        if (base != a[i]) {
+            ans += abs(base - a[i]);
+            cnt--;
+        }
+    }
+
+    cout << ans / 2 << endl;
+}
+
+int main() {
+    // make input and output more efficient
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--) solve();
+    return 0;
+}
