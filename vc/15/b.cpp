6ead15a550e5fd702f32ae3e9a5e042654212040
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2021-09-17 02:11:30 +0900

    vc 15

---
+++ vc/15/b.cpp
@@ -0,0 +1,163 @@
+#include <bits/stdc++.h>
+
+//#include <atcoder/all>
+// using namespace atcoder;
+using namespace std;
+
+// debug
+#define printv(v)         \
+    for (auto x : v) {    \
+        cerr << x << " "; \
+    }                     \
+    cerr << endl;
+
+#define print2d(v, n, m)                \
+    for (int i = 0; i < (n); i++) {     \
+        for (int j = 0; j < (m); j++) { \
+            cerr << v[i][j] << " ";     \
+        }                               \
+        cerr << endl;                   \
+    }
+
+// func
+#define rep(i, last) for (int i = 0; i < (last); i++)
+#define srtv(v) sort((v).begin(), (v).end())
+
+// template
+template <class T>
+void chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+    }
+}
+template <class T>
+void chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+    }
+}
+
+template <class T>
+void out(T x) {
+    cout << x << "\n";
+}
+
+template <class T>
+void out(vector<T> x) {
+    int sz = x.size();
+    for (int i = 0; i < sz; i++) {
+        cout << x[i];
+        if (i < sz - 1) cout << " ";
+    }
+
+    cout << "\n";
+}
+
+// in
+#define rd(...)  \
+    __VA_ARGS__; \
+    read(__VA_ARGS__)
+#define rdv(value, ...) \
+    value(__VA_ARGS__); \
+    cin >> value
+template <class T>
+auto& operator>>(istream& is, vector<T>& xs) {
+    for (auto& x : xs) is >> x;
+    return is;
+}
+template <class T, class Y>
+auto& operator>>(istream& is, vector<pair<T, Y>>& xs) {
+    for (auto& [x1, x2] : xs) is >> x1 >> x2;
+    return is;
+}
+template <class... Args>
+auto& read(Args&... args) {
+    return (cin >> ... >> args);
+}
+
+// Driver function to sort the vector elements
+// by second element of pairs
+bool sort_by_fir_desc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second > b.second);
+}
+bool sort_by_sec_asc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second < b.second);
+}
+bool sort_by_sec_desc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second > b.second);
+}
+
+// type
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef priority_queue<int> pqi;                              // greater
+typedef priority_queue<int, vector<int>, greater<int>> pqli;  // less
+typedef priority_queue<long long, vector<long long>, greater<long long>> pqlll;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+
+// const
+const long long INF_9 = 1001002009;
+
+/*-----------------------------------
+        Coding Starts Here
+------------------------------------*/
+
+void solve() {
+    // in
+    string t;
+    read(t);
+    int a;
+    read(a);
+    if (a == 0) {
+        out(t);
+        return;
+    }
+    int h = (a / 60) % 24;
+    int m = a % 60;
+    // cout << h << " " << m << endl;
+    int mm = (t[3] - '0') * 10 + (t[4] - '0') + m;
+    int hh = (t[0] - '0') * 10 + (t[1] - '0') + h;
+    // cout << hh << " " << mm << endl;
+
+    if (mm >= 60) {
+        hh += 1;
+        mm %= 60;
+    }
+    if (hh >= 24) {
+        hh %= 24;
+    }
+    // cout << hh << " " << mm << endl;
+
+    if (mm < 10) {
+        t[3] = '0';
+    } else {
+        t[3] = mm / 10 + '0';
+    }
+    t[4] = (mm % 10) + '0';
+
+    if (hh < 10) {
+        t[0] = '0';
+    } else {
+        t[0] = hh / 10 + '0';
+    }
+    t[1] = (hh % 10) + '0';
+    out(t);
+}
+
+int main() {
+    // make input and output more efficient
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--) solve();
+    return 0;
+}
