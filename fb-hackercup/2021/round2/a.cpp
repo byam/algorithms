6b672e07486a3fff0d0bd6f1ac62f04ccb0992b8
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2021-09-26 20:51:35 +0900

    fh-r2

---
+++ fb-hackercup/2021/round2/a.cpp
@@ -0,0 +1,173 @@
+#include <bits/stdc++.h>
+
+//#include <atcoder/all>
+// using namespace atcoder;
+using namespace std;
+
+// debug
+#define printv(v)         \
+    for (auto x : v) {    \
+        cerr << x << " "; \
+    }                     \
+    cerr << endl;
+
+#define print2d(v)                              \
+    for (int i = 0; i < v.size(); i++) {        \
+        for (int j = 0; j < v[i].size(); j++) { \
+            cerr << v[i][j] << " ";             \
+        }                                       \
+        cerr << endl;                           \
+    }
+
+// func
+#define rep(i, last) for (int i = 0; i < (last); i++)
+#define srtv(v) sort((v).begin(), (v).end())
+#define all(v) (v).begin(), (v).end()
+
+// template
+template <class T>
+void chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+    }
+}
+template <class T>
+void chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+    }
+}
+
+template <class T>
+void out(T x) {
+    cout << x << "\n";
+}
+
+template <class T>
+void out(vector<T> x) {
+    int sz = x.size();
+    for (int i = 0; i < sz; i++) {
+        cout << x[i];
+        if (i < sz - 1) cout << " ";
+    }
+
+    cout << "\n";
+}
+
+// in
+#define rd(...)  \
+    __VA_ARGS__; \
+    read(__VA_ARGS__)
+#define rdv(value, ...) \
+    value(__VA_ARGS__); \
+    cin >> value
+template <class T>
+auto& operator>>(istream& is, vector<T>& xs) {
+    for (auto& x : xs) is >> x;
+    return is;
+}
+template <class T, class Y>
+auto& operator>>(istream& is, vector<pair<T, Y>>& xs) {
+    for (auto& [x1, x2] : xs) is >> x1 >> x2;
+    return is;
+}
+template <class... Args>
+auto& read(Args&... args) {
+    return (cin >> ... >> args);
+}
+
+// Driver function to sort the vector elements
+// by second element of pairs
+bool sort_by_fir_desc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second > b.second);
+}
+bool sort_by_sec_asc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second < b.second);
+}
+bool sort_by_sec_desc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second > b.second);
+}
+
+// type
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef priority_queue<int> pqi;                              // greater
+typedef priority_queue<int, vector<int>, greater<int>> pqli;  // less
+typedef priority_queue<long long, vector<long long>, greater<long long>> pqlll;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<int>> Graph;
+
+// const
+const long long INF_9 = 1001002009;
+
+/*-----------------------------------
+        Coding Starts Here
+------------------------------------*/
+
+vb used;
+
+void calculate(vi os, vi op, ll& ans) {
+    int m = os.size();
+
+    out(os);
+    out(op);
+
+    auto s = os;
+    auto p = op;
+
+    sort(all(s));
+    sort(all(p));
+
+    vi com(2 * m);
+    auto it =
+        set_difference(s.begin(), s.end(), p.begin(), p.end(), com.begin());
+
+    // int diff = m - (it - com.begin());
+    out(com);
+}
+
+void solve() {
+    // in
+    int n, m;
+    read(n, m);
+    vi s(m);
+    read(s);
+
+    used.clear();
+    used.resize(m, false);
+
+    ll ans = 0;
+    vvi p(n, vi(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            int x;
+            read(x);
+            p[i][j] = x;
+        }
+    }
+
+    for (auto x : p) {
+        calculate(s, x, ans);
+        s = x;
+    }
+
+    out(ans);
+}
+
+int main() {
+    // make input and output more efficient
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
