4f58788730ea13c240565dbdd2aacb0451108b52
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2021-09-28 23:15:51 +0900

    graph

--- aoj/GRL/README.md
+++ aoj/GRL/README.md
@@ -2,24 +2,47 @@
 
 ref: https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/all
 
-- [単一始点最短経路(Dijkstra)](./1a-単一始点最短経路.cpp)
+- [グラフに関するアルゴリズムライブラリ](#グラフに関するアルゴリズムライブラリ)
+  - [最短経路 / Shortest Path](#最短経路--shortest-path)
+  - [全域木 / Spanning Tree](#全域木--spanning-tree)
+  - [連結成分 / Connected Components](#連結成分--connected-components)
+
+## 最短経路 / Shortest Path
+- [単一始点最短経路 / Single Shortest Path / (Dijkstra)](./1a-単一始点最短経路.cpp)
   - 計算量：O(|E|log|V|)
   - 欠点：負の辺があるときに使用できない
   - (有向)
-- [単一始点最短経路（負の重みをもつ辺を含む）(BellmanFord)](./1b-単一始点最短経路-負の重みをもつ辺を含む.cpp)
+- [単一始点最短経路負 / Single Shortest Path with Negative Edges / (BellmanFord)](./1b-単一始点最短経路-負の重みをもつ辺を含む.cpp)
   - 計算量：O(E * V)
   - 利点：負の辺があっても正しく動作し、負の閉路を検出することができる。
   - 欠点：計算量が log ではない
   - (有向)
-- [全点対間最短経路(WarshallFloyd)](./1c-全点対間最短経路.cpp)
+- [全点対間最短経路 / All Pairs Shortest / Path / (WarshallFloyd)](./1c-全点対間最短経路.cpp)
   - 計算量：O(|V|^3)
   - 利点：
     - 負の有向路を含むかどうかを検出できる
     - s から t への最短路のパス
   - (有向)
-- [最小全域木/Minimum Spanning Tree(Kruskal)](2a-最小全域木-Kruskal.cpp)
+
+## 全域木 / Spanning Tree
+- [最小全域木 / Minimum Spanning Tree / (Kruskal)](./2a-最小全域木-Kruskal.cpp)
   - Kruskal :クラスカル法で minimum spanning tree を求める構造体
   - 計算量: O(|E|log|V|)
   - 入力: 辺のvector, 頂点数V
   - 最小全域木の重みの総和: sum
   - (無向)
+- 最小全域有向木 / Minimum-Cost Arborescene / (Chu-Liu/Edmonds)
+
+## 連結成分 / Connected Components
+
+- [関節点 / Articulation Points / Lowlink](./3a-関節点.cpp)
+  - グラフから取り除くと、グラフが非連結になってしまうような頂点
+  - Lowlink:
+    - 計算量: O(E+V)
+    - グラフの関節点・橋を列挙する構造体
+    - 関節点の集合: `vector<int> aps`
+    - 橋の集合: `vector<P> bridges`
+    - グラフ G は連結である
+
+- Bridges
+- Strongly Connected Components
---
+++ atcoder/tags/mst/Built-abc065d.cpp
@@ -0,0 +1,228 @@
+#include <bits/stdc++.h>
+
+//#include <atcoder/all>
+// using namespace atcoder;
+using namespace std;
+
+// debug
+#define printv(v)         \
+    for (auto x : v) {    \
+        cerr << x << " "; \
+    }                     \
+    cerr << endl;
+
+#define print2d(v)                              \
+    for (int i = 0; i < v.size(); i++) {        \
+        for (int j = 0; j < v[i].size(); j++) { \
+            cerr << v[i][j] << " ";             \
+        }                                       \
+        cerr << endl;                           \
+    }
+
+// func
+#define rep(i, last) for (int i = 0; i < (last); i++)
+#define srtv(v) sort((v).begin(), (v).end())
+#define all(v) (v).begin(), (v).end()
+
+// template
+template <class T>
+void chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+    }
+}
+template <class T>
+void chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+    }
+}
+
+template <class T>
+void out(T x) {
+    cout << x << "\n";
+}
+
+template <class T>
+void out(vector<T> x) {
+    int sz = x.size();
+    for (int i = 0; i < sz; i++) {
+        cout << x[i];
+        if (i < sz - 1) cout << " ";
+    }
+
+    cout << "\n";
+}
+
+// in
+#define rd(...)  \
+    __VA_ARGS__; \
+    read(__VA_ARGS__)
+#define rdv(value, ...) \
+    value(__VA_ARGS__); \
+    cin >> value
+template <class T>
+auto& operator>>(istream& is, vector<T>& xs) {
+    for (auto& x : xs) is >> x;
+    return is;
+}
+template <class T, class Y>
+auto& operator>>(istream& is, vector<pair<T, Y>>& xs) {
+    for (auto& [x1, x2] : xs) is >> x1 >> x2;
+    return is;
+}
+template <class... Args>
+auto& read(Args&... args) {
+    return (cin >> ... >> args);
+}
+
+// Driver function to sort the vector elements
+// by second element of pairs
+bool sort_by_fir_desc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second > b.second);
+}
+bool sort_by_sec_asc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second < b.second);
+}
+bool sort_by_sec_desc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second > b.second);
+}
+
+// type
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef priority_queue<int> pqi;                              // greater
+typedef priority_queue<int, vector<int>, greater<int>> pqli;  // less
+typedef priority_queue<long long, vector<long long>, greater<long long>> pqlll;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<int>> Graph;
+
+// const
+const long long INF_9 = 1001002009;
+
+/*-----------------------------------
+        Coding Starts Here
+------------------------------------*/
+/* UnionFind：素集合系管理の構造体(union by rank)
+    isSame(x, y): x と y が同じ集合にいるか。 計算量はならし O(α(n))
+    unite(x, y): x と y を同じ集合にする。計算量はならし O(α(n))
+*/
+struct UnionFind {  // The range of node number is u 0 v n-1
+    vector<int> rank, parents;
+    UnionFind() {}
+    UnionFind(int n) {  // make n trees.
+        rank.resize(n, 0);
+        parents.resize(n, 0);
+        for (int i = 0; i < n; i++) {
+            makeTree(i);
+        }
+    }
+    void makeTree(int x) {
+        parents[x] = x;  // the parent of x is x
+        rank[x] = 0;
+    }
+    bool isSame(int x, int y) { return findRoot(x) == findRoot(y); }
+    void unite(int x, int y) {
+        x = findRoot(x);
+        y = findRoot(y);
+        if (rank[x] > rank[y]) {
+            parents[y] = x;
+        } else {
+            parents[x] = y;
+            if (rank[x] == rank[y]) {
+                rank[y]++;
+            }
+        }
+    }
+    int findRoot(int x) {
+        if (x != parents[x]) parents[x] = findRoot(parents[x]);
+        return parents[x];
+    }
+};
+
+// 辺の定義
+struct Edge {
+    ll u;
+    ll v;
+    ll cost;
+};
+
+bool comp_e(const Edge& e1, const Edge& e2) {
+    return e1.cost < e2.cost;
+}  // 辺を直接比較するための関数
+
+/* 最小全域木
+    Kruskal :クラスカル法で minimum spanning tree を求める構造体
+    入力: 辺のvector, 頂点数V
+    最小全域木の重みの総和: sum
+    計算量: O(|E|log|V|)
+    ref: https://algo-logic.info/kruskal-mst/
+*/
+struct Kruskal {
+    UnionFind uft;
+    ll sum;  // 最小全域木の重みの総和
+    vector<Edge> edges;
+    int V;
+    Kruskal(const vector<Edge>& edges_, int V_) : edges(edges_), V(V_) {
+        init();
+    }
+    void init() {
+        sort(edges.begin(), edges.end(), comp_e);  // 辺の重みでソート
+        uft = UnionFind(V);
+        sum = 0;
+        for (auto e : edges) {
+            if (!uft.isSame(e.u, e.v)) {  // 閉路にならなければ加える
+                uft.unite(e.u, e.v);
+                sum += e.cost;
+            }
+        }
+    }
+};
+
+void solve() {
+    // in
+    int n;
+    read(n);
+    vpii vx, vy;
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        read(x, y);
+        vx.push_back({x, i});
+        vy.push_back({y, i});
+    }
+
+    sort(all(vx));
+    sort(all(vy));
+
+    // only valuable edges
+    vector<Edge> edges;
+
+    for (int i = 1; i < n; i++) {
+        edges.push_back(
+            {vx[i - 1].second, vx[i].second, vx[i].first - vx[i - 1].first});
+        edges.push_back(
+            {vy[i - 1].second, vy[i].second, vy[i].first - vy[i - 1].first});
+    }
+
+    // mst
+    Kruskal mst(edges, n);
+    out(mst.sum);
+}
+
+int main() {
+    // make input and output more efficient
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--) solve();
+    return 0;
+}
