c55d8d90d540960e0ea5d7d4a102676ca2408483
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2021-09-28 14:26:24 +0900

    graph

---
+++ byalib/graph/単一始点最短経路-Dijkstra.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> P;
+
+const long long INF = 1LL << 60;
+
+// 辺を表す型
+struct Edge {
+    ll to, cost;  // 辺の接続先頂点, 辺の重み
+    Edge(ll to, ll cost) : to(to), cost(cost) {}  // コンストラクタ
+};
+
+typedef vector<vector<Edge>> GraphEdge;
+
+/* dijkstra(G,s,dis)
+    入力：グラフ G, 開始点 s, 距離を格納する dis
+    計算量：O(|E|log|V|)
+    副作用：dis が書き換えられる
+    欠点：負の辺があるときに使用できない（その場合はBellman-Fordを利用）
+    ref: https://algo-logic.info/dijkstra/
+*/
+void dijkstra(const GraphEdge &G, int s, vector<ll> &dis) {
+    int N = G.size();
+    dis.resize(N, INF);
+    priority_queue<P, vector<P>, greater<P>>
+        pq;  // 「仮の最短距離, 頂点」が小さい順に並ぶ
+    dis[s] = 0;
+    pq.emplace(dis[s], s);
+    while (!pq.empty()) {
+        P p = pq.top();
+        pq.pop();
+        int v = p.second;
+        if (dis[v] < p.first) {  // 最短距離で無ければ無視
+            continue;
+        }
+        for (auto &e : G[v]) {
+            if (dis[e.to] >
+                dis[v] + e.cost) {  // 最短距離候補なら priority_queue に追加
+                dis[e.to] = dis[v] + e.cost;
+                pq.emplace(dis[e.to], e.to);
+            }
+        }
+    }
+}
+
+int main() {
+    // 入力
+    int V, E, r;
+    cin >> V >> E >> r;
+
+    GraphEdge G(V);
+    for (int i = 0; i < E; i++) {
+        int s, t, d;
+        cin >> s >> t >> d;
+        G[s].push_back(Edge(t, d));
+    }
+
+    // 計算
+    vector<ll> dist;
+    dijkstra(G, r, dist);
+
+    // 出力
+    for (auto x : dist) {
+        if (x == INF)
+            printf("INF\n");
+        else
+            printf("%d\n", x);
+    }
+    return 0;
+}
---
+++ byalib/graph/単一始点最短経路-負の重みをもつ辺を含む-BellmanFord.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> P;
+
+const long long INF = 1LL << 60;
+
+// 辺を表す型
+struct Edge {
+    ll to, cost;  // 辺の接続先頂点, 辺の重み
+    Edge(ll to, ll cost) : to(to), cost(cost) {}  // コンストラクタ
+};
+
+typedef vector<vector<Edge>> GraphEdge;
+
+/*
+    単一始点最短経路（負の重みをもつ辺を含む）
+
+    計算量：O(E * V)
+
+    負の辺があっても正しく動作し、負の閉路を検出することができる。
+    戻り値がtrueなら負の閉路を含む
+ */
+bool bellman_ford(GraphEdge& graph, int s, vector<ll>& dist) {
+    int n = graph.size();
+    dist = vector<ll>(n, INF);
+    dist[s] = 0;  // 開始点の距離は0
+    for (int i = 0; i < n; i++) {
+        for (int v = 0; v < n; v++) {
+            for (int k = 0; k < graph[v].size(); k++) {
+                Edge e = graph[v][k];
+                if (dist[v] != INF && dist[e.to] > dist[v] + e.cost) {
+                    dist[e.to] = dist[v] + e.cost;
+                    if (i == n - 1)
+                        return true;  // n回目にも更新があるなら負の閉路が存在
+                }
+            }
+        }
+    }
+    return false;
+}
+
+int main() {
+    // 入力
+    int V, E, r;
+    cin >> V >> E >> r;
+
+    GraphEdge G(V);
+    for (int i = 0; i < E; i++) {
+        int s, t, d;
+        cin >> s >> t >> d;
+        G[s].push_back(Edge(t, d));
+    }
+
+    // 計算
+    vector<ll> dist;
+    bool neg_cycle = bellman_ford(G, r, dist);
+    if (neg_cycle) {
+        printf("NEGATIVE CYCLE\n");
+        return 0;
+    }
+
+    // 出力
+    for (auto x : dist) {
+        if (x == INF)
+            printf("INF\n");
+        else
+            printf("%d\n", x);
+    }
+    return 0;
+}
