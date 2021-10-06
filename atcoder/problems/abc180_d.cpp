8be062d66744b46bf98d4f8fd8c5f2ba6324baef
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2021-10-07 02:10:20 +0900

    daily

---
+++ atcoder/problems/abc180_d.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+
+//#include <atcoder/all>
+// using namespace atcoder;
+using namespace std;
+
+// func
+#define rep(i, last) for (int i = 0; i < (last); i++)
+#define srtv(v) sort((v).begin(), (v).end())
+#define all(v) (v).begin(), (v).end()
+
+// template
+template <class T>
+void chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+    }
+}
+template <class T>
+void chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+    }
+}
+
+// out -> print
+template <class T>
+void out(T x) {
+    cout << x << "\n";
+}
+
+template <class T>
+void out(vector<T> x) {
+    int sz = x.size();
+    for (int i = 0; i < sz; i++) {
+        cout << x[i];
+        if (i < sz - 1) cout << " ";
+    }
+
+    cout << "\n";
+}
+
+template <class T>
+void out(vector<pair<T, T>> p) {
+    for (auto [f, s] : p) {
+        cout << f << " " << s << endl;
+    }
+}
+
+template <class T>
+void out(map<T, T> m) {
+    for (auto [k, v] : m) {
+        cout << k << ": " << v << endl;
+    }
+}
+
+template <class T>
+void out(set<T> s) {
+    for (auto x : s) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+// in
+#define rd(...)  \
+    __VA_ARGS__; \
+    read(__VA_ARGS__)
+#define rdv(value, ...) \
+    value(__VA_ARGS__); \
+    cin >> value
+template <class T>
+auto& operator>>(istream& is, vector<T>& xs) {
+    for (auto& x : xs) is >> x;
+    return is;
+}
+template <class T, class Y>
+auto& operator>>(istream& is, vector<pair<T, Y>>& xs) {
+    for (auto& [x1, x2] : xs) is >> x1 >> x2;
+    return is;
+}
+template <class... Args>
+auto& read(Args&... args) {
+    return (cin >> ... >> args);
+}
+
+// Driver function to sort the vector elements
+// by second element of pairs
+bool sort_by_fir_desc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second > b.second);
+}
+bool sort_by_sec_asc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second < b.second);
+}
+bool sort_by_sec_desc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second > b.second);
+}
+
+// type
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef priority_queue<int> pqi;                              // greater
+typedef priority_queue<int, vector<int>, greater<int>> pqli;  // less
+typedef priority_queue<long long, vector<long long>, greater<long long>> pqlll;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<int>> Graph;
+
+// const
+const long long INF_9 = 1001002009;
+
+/*-----------------------------------
+        Coding Starts Here
+------------------------------------*/
+
+void solve() {
+    // in
+    ll x, y, a, b;
+    read(x, y, a, b);
+    ll ans = 0;
+    // while ((double)a * x <= 2e18 && a * x <= x + b && a * x < y) {
+    //     x *= a;
+    //     ans++;
+    // }
+    while (true) {
+        // prevent overflow
+        if (y / a < x) break;
+
+        if (a * x >= y) break;
+        if (a * x >= b + x) break;
+        x *= a;
+        ans++;
+    }
+    ans += (y - 1 - x) / b;
+    out(ans);
+}
+
+int main() {
+    // make input and output more efficient
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--) solve();
+    return 0;
+}
---
+++ atcoder/problems/abc190_d.cpp
@@ -0,0 +1,160 @@
+#include <bits/stdc++.h>
+
+//#include <atcoder/all>
+// using namespace atcoder;
+using namespace std;
+
+// func
+#define rep(i, last) for (int i = 0; i < (last); i++)
+#define srtv(v) sort((v).begin(), (v).end())
+#define all(v) (v).begin(), (v).end()
+
+// template
+template <class T>
+void chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+    }
+}
+template <class T>
+void chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+    }
+}
+
+// out -> print
+template <class T>
+void out(T x) {
+    cout << x << "\n";
+}
+
+template <class T>
+void out(vector<T> x) {
+    int sz = x.size();
+    for (int i = 0; i < sz; i++) {
+        cout << x[i];
+        if (i < sz - 1) cout << " ";
+    }
+
+    cout << "\n";
+}
+
+template <class T>
+void out(vector<pair<T, T>> p) {
+    for (auto [f, s] : p) {
+        cout << f << " " << s << endl;
+    }
+}
+
+template <class T>
+void out(map<T, T> m) {
+    for (auto [k, v] : m) {
+        cout << k << ": " << v << endl;
+    }
+}
+
+template <class T>
+void out(set<T> s) {
+    for (auto x : s) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+// in
+#define rd(...)  \
+    __VA_ARGS__; \
+    read(__VA_ARGS__)
+#define rdv(value, ...) \
+    value(__VA_ARGS__); \
+    cin >> value
+template <class T>
+auto& operator>>(istream& is, vector<T>& xs) {
+    for (auto& x : xs) is >> x;
+    return is;
+}
+template <class T, class Y>
+auto& operator>>(istream& is, vector<pair<T, Y>>& xs) {
+    for (auto& [x1, x2] : xs) is >> x1 >> x2;
+    return is;
+}
+template <class... Args>
+auto& read(Args&... args) {
+    return (cin >> ... >> args);
+}
+
+// Driver function to sort the vector elements
+// by second element of pairs
+bool sort_by_fir_desc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second > b.second);
+}
+bool sort_by_sec_asc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second < b.second);
+}
+bool sort_by_sec_desc(const pair<int, int>& a, const pair<int, int>& b) {
+    return (a.second > b.second);
+}
+
+// type
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef priority_queue<int> pqi;                              // greater
+typedef priority_queue<int, vector<int>, greater<int>> pqli;  // less
+typedef priority_queue<long long, vector<long long>, greater<long long>> pqlll;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<int>> Graph;
+
+// const
+const long long INF_9 = 1001002009;
+
+/*-----------------------------------
+        Coding Starts Here
+------------------------------------*/
+
+// function to print the divisors
+set<ll> getDivisors(ll n) {
+    // Vector to store half of the divisors
+    set<ll> v;
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            v.insert(i);
+            v.insert(n / i);
+        }
+    }
+    return v;
+}
+
+void solve() {
+    // in
+    ll n;
+    read(n);
+    set<ll> s = getDivisors(n * 2);
+    // out(s);
+    set<ll> ans;
+    for (auto x : s) {
+        ll a = 2 * n / x - x + 1;
+        if (a % 2) continue;
+        ans.insert(a / 2);
+    }
+    // out(ans);
+    out(ans.size());
+}
+
+int main() {
+    // make input and output more efficient
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--) solve();
+    return 0;
+}
