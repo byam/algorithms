ea0f1f7441592f158d20f5ac16fd14bc53ad5deb
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2021-10-16 20:48:34 +0900

    daily

---
+++ codechef/starters15/2.cpp
@@ -0,0 +1,202 @@
+#include <bits/stdc++.h>
+
+//#include <atcoder/all>
+// using namespace atcoder;
+using namespace std;
+
+// func
+#define rep(i, last) for (int i = 0; i < (last); i++)
+#define srtv(v) sort((v).begin(), (v).end())
+#define all(v) (v).begin(), (v).end()
+
+// template
+template <class T>
+void chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+    }
+}
+template <class T>
+void chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+    }
+}
+
+// out -> print
+template <class T>
+void out(T x) {
+    cout << x << "\n";
+}
+
+template <class T>
+void out(vector<T> x) {
+    int sz = x.size();
+    for (int i = 0; i < sz; i++) {
+        cout << x[i];
+        if (i < sz - 1) cout << " ";
+    }
+
+    cout << "\n";
+}
+
+template <class T>
+void out(vector<pair<T, T>> p) {
+    for (auto [f, s] : p) {
+        cout << f << " " << s << endl;
+    }
+}
+
+template <class T>
+void out(map<T, T> m) {
+    for (auto [k, v] : m) {
+        cout << k << ": " << v << endl;
+    }
+}
+
+template <class T>
+void out(set<T> s) {
+    for (auto x : s) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+// パラメータパックが空になったら終了
+void outt() { cout << endl; }
+
+// ひとつ以上のパラメータを受け取るようにし、
+// 可変引数を先頭とそれ以外に分割する
+template <class Head, class... Tail>
+void outt(Head&& head, Tail&&... tail) {
+    std::cout << head << " ";
+
+    // パラメータパックtailをさらにheadとtailに分割する
+    outt(std::forward<Tail>(tail)...);
+}
+
+// in
+#define rd(...)  \
+    __VA_ARGS__; \
+    read(__VA_ARGS__)
+#define rdv(value, ...) \
+    value(__VA_ARGS__); \
+    cin >> value
+template <class T>
+auto& operator>>(istream& is, vector<T>& xs) {
+    for (auto& x : xs) is >> x;
+    return is;
+}
+template <class T>
+auto& operator<<(ostream& os, vector<T>& xs) {
+    int sz = xs.size();
+    rep(i, sz) os << xs[i] << " \n"[i + 1 == sz];
+    return os;
+}
+template <class T, class Y>
+auto& operator<<(ostream& os, pair<T, Y>& xs) {
+    os << "{" << xs.first << ", " << xs.second << "}";
+    return os;
+}
+template <class T, class Y>
+auto& operator>>(istream& is, vector<pair<T, Y>>& xs) {
+    for (auto& [x1, x2] : xs) is >> x1 >> x2;
+    return is;
+}
+template <class... Args>
+auto& read(Args&... args) {
+    return (cin >> ... >> args);
+}
+
+// type
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef priority_queue<int> pqi;                              // greater
+typedef priority_queue<int, vector<int>, greater<int>> pqli;  // less
+typedef priority_queue<long long, vector<long long>, greater<long long>> pqlll;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<int>> Graph;
+
+// const
+const long long INF_9 = 1001002009;
+
+/*-----------------------------------
+        Coding Starts Here
+------------------------------------*/
+// function to print the divisors
+// A function to print all prime
+// factors of a given number n
+vector<int> primeFactors(int n) {
+    vector<int> f;
+
+    // Print the number of 2s that divide n
+    while (n % 2 == 0) {
+        n = n / 2;
+        f.push_back(2);
+    }
+
+    // n must be odd at this point. So we can skip
+    // one element (Note i = i +2)
+    for (int i = 3; i <= sqrt(n); i = i + 2) {
+        // While i divides n, print i and divide n
+        while (n % i == 0) {
+            n = n / i;
+            f.push_back(i);
+        }
+    }
+
+    // This condition is to handle the case when n
+    // is a prime number greater than 2
+    if (n > 2) f.push_back(n);
+
+    return f;
+}
+
+void solve() {
+    // in
+    ll rd(n, a, b);
+    ll ans = 0;
+
+    vector<int> f = primeFactors(n);
+    int cnt2 = 0;
+    for (auto x : f)
+        if (x == 2) cnt2++;
+    int cntElse = f.size() - cnt2;
+
+    if (a >= 0 and b >= 0) {
+        ans += a * cnt2;
+        ans += b * cntElse;
+    } else if (a >= 0 and b < 0) {
+        if (cnt2 > 0)
+            ans += a * cnt2;
+        else if (cntElse > 0)
+            ans += b * 1;
+    } else if (a < 0 and b >= 0) {
+        ans += b * cntElse;
+        if (cnt2 > 0) ans += a * 1;
+    } else {
+        if (n % 2 == 0)
+            ans = a;
+        else
+            ans = b;
+    }
+    out(ans);
+}
+
+int main() {
+    // make input and output more efficient
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
