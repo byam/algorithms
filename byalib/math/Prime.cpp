2dad272504d1498bd3726657a5ecc579ccd20ded
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2021-10-29 22:08:58 +0900

    factors

--- byalib/math/Prime.cpp
+++ byalib/math/Prime.cpp
@@ -33,6 +33,33 @@ vector<int> findDivisors(int n) {
     return divs;
 }
 
+// A function to print all prime factors of a given number n
+vector<int> primeFactors(int n) {
+    vector<int> f;
+
+    // Print the number of 2s that divide n
+    while (n % 2 == 0) {
+        f.push_back(2);
+        n = n / 2;
+    }
+
+    // n must be odd at this point.  So we can skip
+    // one element (Note i = i +2)
+    for (int i = 3; i <= sqrt(n); i = i + 2) {
+        // While i divides n, print i and divide n
+        while (n % i == 0) {
+            f.push_back(i);
+            n = n / i;
+        }
+    }
+
+    // This condition is to handle the case when n
+    // is a prime number greater than 2
+    if (n > 2) f.push_back(n);
+
+    return f;
+}
+
 // Driver code
 int main() {
     // GCD
