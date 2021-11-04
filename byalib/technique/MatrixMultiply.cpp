35d1b24c74e20d9ac3ee85d6ab1abfd59a7bcdd5
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2021-11-05 02:30:01 +0900

    daily

---
+++ byalib/technique/MatrixMultiply.cpp
@@ -0,0 +1,198 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
+
+//#include <atcoder/all>
+// using namespace atcoder;
+using namespace std;
+
+// func
+#define rep(i, first, last) for (int i = (first); i < (last); i++)
+#define rrep(i, last, first) for (int i = (last); i >= (first); i--)
+#define srtv(v) sort((v).begin(), (v).end())
+#define all(v) (v).begin(), (v).end()
+
+// template
+template <class T>
+void chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+    }
+}
+template <class T>
+void chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+    }
+}
+
+// out -> print
+template <class T>
+void out(T x) {
+    cout << x << "\n";
+}
+
+template <class T>
+void out(vector<T> x) {
+    int sz = x.size();
+    for (int i = 0; i < sz; i++) {
+        cout << x[i];
+        if (i < sz - 1) cout << " ";
+    }
+
+    cout << "\n";
+}
+
+template <class T>
+void out(vector<pair<T, T>> p) {
+    for (auto [f, s] : p) {
+        cout << f << " " << s << endl;
+    }
+}
+
+template <class T>
+void out(map<T, T> m) {
+    for (auto [k, v] : m) {
+        cout << k << ": " << v << endl;
+    }
+}
+
+template <class T>
+void out(set<T> s) {
+    for (auto x : s) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+// パラメータパックが空になったら終了
+void outt() { cout << endl; }
+
+// ひとつ以上のパラメータを受け取るようにし、
+// 可変引数を先頭とそれ以外に分割する
+template <class Head, class... Tail>
+void outt(Head&& head, Tail&&... tail) {
+    std::cout << head << " ";
+
+    // パラメータパックtailをさらにheadとtailに分割する
+    outt(std::forward<Tail>(tail)...);
+}
+
+// in
+#define rd(...)  \
+    __VA_ARGS__; \
+    read(__VA_ARGS__)
+#define rdv(value, ...) \
+    value(__VA_ARGS__); \
+    cin >> value
+template <class T>
+auto& operator>>(istream& is, vector<T>& xs) {
+    for (auto& x : xs) is >> x;
+    return is;
+}
+template <class T>
+auto& operator<<(ostream& os, vector<T>& xs) {
+    int sz = xs.size();
+    rep(i, 0, sz) os << xs[i] << " \n"[i + 1 == sz];
+    return os;
+}
+template <class T, class Y>
+auto& operator<<(ostream& os, pair<T, Y>& xs) {
+    os << "{" << xs.first << ", " << xs.second << "}";
+    return os;
+}
+template <class T, class Y>
+auto& operator>>(istream& is, vector<pair<T, Y>>& xs) {
+    for (auto& [x1, x2] : xs) is >> x1 >> x2;
+    return is;
+}
+template <class... Args>
+auto& read(Args&... args) {
+    return (cin >> ... >> args);
+}
+
+// type
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef priority_queue<int> pqi;                              // greater
+typedef priority_queue<int, vector<int>, greater<int>> pqli;  // less
+typedef priority_queue<long long, vector<long long>, greater<long long>> pqlll;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<int>> Graph;
+
+// const
+const ll MOD = 1000000007;
+const ll INF = 1e18;
+
+/*-----------------------------------
+        Coding Starts Here
+------------------------------------*/
+
+ll mod_mul(ll a, ll b) { return ((a % MOD) * (b % MOD)) % MOD; }
+ll mod_add(ll a, ll b) { return ((a % MOD) + (b % MOD)) % MOD; }
+
+void mat_mul(ll F[2][2], ll M[2][2]) {
+    ll x = mod_add(mod_mul(F[0][0], M[0][0]), mod_mul(F[0][1], M[1][0]));
+    ll y = mod_add(mod_mul(F[0][0], M[0][1]), mod_mul(F[0][1], M[1][1]));
+    ll z = mod_add(mod_mul(F[1][0], M[0][0]), mod_mul(F[1][1], M[1][0]));
+    ll w = mod_add(mod_mul(F[1][0], M[0][1]), mod_mul(F[1][1], M[1][1]));
+
+    M[0][0] = x;
+    M[0][1] = y;
+    M[1][0] = z;
+    M[1][1] = w;
+}
+
+void mat_power(ll F[2][2], ll n, ll a, ll b) {
+    ll M[2][2] = {{a, b}, {0, 1}};
+
+    if (n == 1) return;
+
+    mat_power(F, n / 2, a, b);
+    mat_mul(F, F);
+
+    if (n % 2 != 0) mat_mul(M, F);
+}
+
+// Function that returns nth Fibonacci number
+ll fib(ll a, ll b, ll n, ll x) {
+    ll F[2][2] = {{a, b}, {0, 1}};
+    ll M[2][2] = {{x, 0}, {1, 0}};
+
+    mat_power(F, n, a, b);
+
+    mat_mul(F, M);
+    return M[0][0];
+}
+
+void solve() {
+    // in
+    ll rd(a, b, n, x);
+
+    out(fib(a, b, n, x));
+}
+
+// ref:
+// https://medium.com/codebrace/starred-problem-2-nth-fibonacci-number-in-log-n-time-821ea9a18296
+
+int main() {
+    // make input and output more efficient
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--) solve();
+    return 0;
+}
