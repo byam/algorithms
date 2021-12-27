d12d6ae88c40ee6e2be9ec5688559a7894af24ef
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2021-12-27 16:56:24 +0900

    daily

---
+++ codechef/b.cpp
@@ -0,0 +1,214 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
+
+//#include <atcoder/all>
+// using namespace atcoder;
+using namespace std;
+
+// func
+#define rep(i, first, last) for (int i = (first); i < (last); i++)
+#define rrep(i, last, first) for (int i = (last); i >= (first); i--)
+#define srtv(v) sort((v).begin(), (v).end())
+#define all(v) (v).begin(), (v).end()
+
+// template
+template <class T>
+void chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+    }
+}
+template <class T>
+void chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+    }
+}
+
+// out -> print
+template <class T>
+void out(T x) {
+    cout << x << "\n";
+}
+
+template <class T>
+void out(vector<T> x) {
+    int sz = x.size();
+    for (int i = 0; i < sz; i++) {
+        cout << x[i];
+        if (i < sz - 1) cout << " ";
+    }
+
+    cout << "\n";
+}
+
+template <class T>
+void out(vector<pair<T, T>> p) {
+    for (auto [f, s] : p) {
+        cout << f << " " << s << endl;
+    }
+}
+
+template <class T>
+void out(map<T, T> m) {
+    for (auto [k, v] : m) {
+        cout << k << ": " << v << endl;
+    }
+}
+
+template <class T>
+void out(set<T> s) {
+    for (auto x : s) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+// パラメータパックが空になったら終了
+void outt() { cout << endl; }
+
+// ひとつ以上のパラメータを受け取るようにし、
+// 可変引数を先頭とそれ以外に分割する
+template <class Head, class... Tail>
+void outt(Head&& head, Tail&&... tail) {
+    std::cout << head << " ";
+
+    // パラメータパックtailをさらにheadとtailに分割する
+    outt(std::forward<Tail>(tail)...);
+}
+
+// in
+#define rd(...)  \
+    __VA_ARGS__; \
+    read(__VA_ARGS__)
+#define rdv(value, ...) \
+    value(__VA_ARGS__); \
+    cin >> value
+template <class T>
+auto& operator>>(istream& is, vector<T>& xs) {
+    for (auto& x : xs) is >> x;
+    return is;
+}
+template <class T>
+auto& operator<<(ostream& os, vector<T>& xs) {
+    int sz = xs.size();
+    rep(i, 0, sz) os << xs[i] << " \n"[i + 1 == sz];
+    return os;
+}
+template <class T, class Y>
+auto& operator<<(ostream& os, pair<T, Y>& xs) {
+    os << "{" << xs.first << ", " << xs.second << "}";
+    return os;
+}
+template <class T, class Y>
+auto& operator>>(istream& is, vector<pair<T, Y>>& xs) {
+    for (auto& [x1, x2] : xs) is >> x1 >> x2;
+    return is;
+}
+template <class... Args>
+auto& read(Args&... args) {
+    return (cin >> ... >> args);
+}
+
+// type
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef priority_queue<int> pqi;                              // greater
+typedef priority_queue<int, vector<int>, greater<int>> pqli;  // less
+typedef priority_queue<long long, vector<long long>, greater<long long>> pqlll;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<int>> Graph;
+
+// const
+const ll MOD = 1000000007;
+const ll INF = 1e18;
+
+/*-----------------------------------
+        Coding Starts Here
+------------------------------------*/
+
+// Function to return the sum of the count
+// of set bits in the integers from 1 to n
+int countSetBits(int n, map<int, int>& m) {
+    // Ignore 0 as all the bits are unset
+    n++;
+
+    // To store the powers of 2
+    int powerOf2 = 2;
+
+    // To store the result, it is initialized
+    // with n/2 because the count of set
+    // least significant bits in the integers
+    // from 1 to n is n/2
+    int ans = n / 2;
+
+    m[1] = ans;
+
+    // Loop for every bit required to represent n
+    while (powerOf2 <= n) {
+        int cnt = 0;
+        // Total count of pairs of 0s and 1s
+        int totalPairs = n / powerOf2;
+
+        // totalPairs/2 gives the complete
+        // count of the pairs of 1s
+        // Multiplying it with the current power
+        // of 2 will give the count of
+        // 1s in the current bit
+        cnt += (totalPairs / 2) * powerOf2;
+
+        // If the count of pairs was odd then
+        // add the remaining 1s which could
+        // not be groupped together
+        cnt += (totalPairs & 1) ? (n % powerOf2) : 0;
+
+        // outt(powerOf2, cnt);
+        m[powerOf2] = cnt;
+
+        // Next power of 2
+        powerOf2 <<= 1;
+
+        ans += cnt;
+    }
+
+    // Return the result
+    return ans;
+}
+
+void solve() {
+    // in
+    int rd(l, r);
+    map<int, int> ml, mr;
+    countSetBits(r, mr);
+    if (l > 1) countSetBits(l - 1, ml);
+    int ma = 0;
+    for (auto [k, v] : ml) {
+        mr[k] -= v;
+    }
+    for (auto [k, v] : mr) chmax(ma, v);
+    // out(mr);
+    out(r - l + 1 - ma);
+}
+
+int main() {
+    // make input and output more efficient
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
