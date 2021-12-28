457465df2a70a24bbe63810a6c23ee26d9d555d6
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2021-12-28 12:46:59 +0900

    daily

---
+++ codeforces/edu120/c.cpp
@@ -0,0 +1,203 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
+
+//#include <atcoder/all>
+// using namespace atcoder;
+using namespace std;
+
+// func
+#define rep(i, first, last) for (int i = (first); i < (last); i++)
+#define rrep(i, last, first) for (int i = (last); i >= (first); i--)
+#define srtv(v) sort((v).begin(), (v).end())
+#define all(v) (v).begin(), (v).end()
+
+// template
+template <class T>
+void chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+    }
+}
+template <class T>
+void chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+    }
+}
+
+// out -> print
+template <class T>
+void out(T x) {
+    cout << x << "\n";
+}
+
+template <class T>
+void out(vector<T> x) {
+    int sz = x.size();
+    for (int i = 0; i < sz; i++) {
+        cout << x[i];
+        if (i < sz - 1) cout << " ";
+    }
+
+    cout << "\n";
+}
+
+template <class T>
+void out(vector<pair<T, T>> p) {
+    for (auto [f, s] : p) {
+        cout << f << " " << s << endl;
+    }
+}
+
+template <class T>
+void out(map<T, T> m) {
+    for (auto [k, v] : m) {
+        cout << k << ": " << v << endl;
+    }
+}
+
+template <class T>
+void out(set<T> s) {
+    for (auto x : s) {
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+// パラメータパックが空になったら終了
+void outt() { cout << endl; }
+
+// ひとつ以上のパラメータを受け取るようにし、
+// 可変引数を先頭とそれ以外に分割する
+template <class Head, class... Tail>
+void outt(Head&& head, Tail&&... tail) {
+    std::cout << head << " ";
+
+    // パラメータパックtailをさらにheadとtailに分割する
+    outt(std::forward<Tail>(tail)...);
+}
+
+// in
+#define rd(...)  \
+    __VA_ARGS__; \
+    read(__VA_ARGS__)
+#define rdv(value, ...) \
+    value(__VA_ARGS__); \
+    cin >> value
+template <class T>
+auto& operator>>(istream& is, vector<T>& xs) {
+    for (auto& x : xs) is >> x;
+    return is;
+}
+template <class T>
+auto& operator<<(ostream& os, vector<T>& xs) {
+    int sz = xs.size();
+    rep(i, 0, sz) os << xs[i] << " \n"[i + 1 == sz];
+    return os;
+}
+template <class T, class Y>
+auto& operator<<(ostream& os, pair<T, Y>& xs) {
+    os << "{" << xs.first << ", " << xs.second << "}";
+    return os;
+}
+template <class T, class Y>
+auto& operator>>(istream& is, vector<pair<T, Y>>& xs) {
+    for (auto& [x1, x2] : xs) is >> x1 >> x2;
+    return is;
+}
+template <class... Args>
+auto& read(Args&... args) {
+    return (cin >> ... >> args);
+}
+
+// type
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<long long> vll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef priority_queue<int> pqi;                              // greater
+typedef priority_queue<int, vector<int>, greater<int>> pqli;  // less
+typedef priority_queue<long long, vector<long long>, greater<long long>> pqlll;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+typedef vector<vector<int>> Graph;
+
+// const
+const ll MOD = 1000000007;
+const ll INF = 1e18;
+
+/*-----------------------------------
+        Coding Starts Here
+------------------------------------*/
+
+void solve() {
+    // in
+    ll rd(n, k);
+    vll rdv(a, n);
+    sort(all(a));
+    int j = n - 1;
+    ll sum = 0;
+    for (auto x : a) sum += x;
+
+    if (sum <= k) {
+        out(0);
+        return;
+    }
+
+    if (a[0] * n <= k) {
+        int ans = 0;
+        int i = n - 1;
+        while (sum > k) {
+            sum -= a[i] - a[0];
+            i--;
+            ans++;
+        }
+        out(ans);
+        return;
+    }
+
+    ll ans = a[0] - k / n + n - 1;
+    if (n == 1) {
+        out(ans);
+        return;
+    }
+    if (n == 2) {
+        ans = a[0] - k / 2 + 1;
+        out(ans);
+        return;
+    }
+
+    int cnt = 0;
+    for (int i = n - 1; i >= 1; i--) {
+        sum -= a[i] - a[0];
+        cnt++;
+        ll d = cnt;
+        if (k >= sum)
+            chmin(ans, d);
+        else {
+            d += (sum - k) / cnt;
+            chmin(ans, d);
+        }
+    }
+    out(ans);
+}
+
+int main() {
+    // make input and output more efficient
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
